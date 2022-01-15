0934b6a20e973f0d0ad34d4aa3749a1f9b15c936
Beyar <beyaralhaji@gmail.com>
2022-01-15 11:16:25 +0000

    initial user authentication

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const data = require("./data");
 const app = express();
-
+const bcrypt = require("bcrypt");
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -15,7 +15,7 @@ const port = 3100;
 
 app.get("/api/items", async (request, response) => {
   const items = await data.getAllItems();
-  
+
   response.status(200).json(items);
 });
 
@@ -27,6 +27,46 @@ app.post("/api/items", async (request, response) => {
   await data.addNewItem(item);
 
   response.status(201).send(`Item added with task: ${item.task}`);
+});
+
+app.post("/api/items/markdone", async (request, response) => {
+  const item = {
+    task: request.body.task,
+  };
+  await data.markDone(item);
+
+  response.status(201).send(`Item updated with task: ${item.task}`);
+});
+
+app.post("/api/items/undo", async (req, res) => {
+  const item = {
+    task: req.body.task,
+  };
+  await data.markUndo(item);
+  res.status(201).send(`item updated with task ${item.task}`);
+});
+
+app.post("/api/items/delete", async (req, res) => {
+  await data.deleteItem(req.body.task);
+  res.status(201).send(`item with task ${req.body.task} deleted`);
+});
+
+app.post("/api/user", async (request, response) => {
+  const user = request.body.user;
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  await data.addNewUser(user.username, hashedPassword);
+
+  response.status(201).send(`user added with name: ${user.username}`);
+});
+
+app.post("/api/user/auth", async (req, res) => {
+  const user = req.body.user;
+  const checkUser = await data.getUser(user.username, user.password);
+  if (checkUser) {
+    res.status(201).send(`authorized user`);
+  } else {
+    res.status(404).send(`un authorized user`);
+  }
 });
 
 app.listen(port, () => {
--- src/data.js
+++ src/data.js
@@ -32,5 +32,51 @@ const addNewItem = async (item) => {
   const result = await client.query(query, [item.task]);
 };
 
+const markDone = async (item) => {
+  const query = "update items set complete = true where task = $1::text";
+  const result = await client.query(query, [item.task]);
+};
+
+const markUndo = async (item) => {
+  const query = "update items set complete = false where task = $1::text";
+  const result = await client.query(query, [item.task]);
+};
+
+const deleteItem = async (task) => {
+  const query = "delete from items where task = $1::text";
+  const result = await client.query(query, [task]);
+};
+
+const addNewUser = async (username, password) => {
+  const query =
+    "insert into users (user_name, user_password) values ($1::text, $2::text);";
+  const result = await client.query(query, [username, password]);
+};
+
+const getUser = async (username, password) => {
+  const query =
+    "select * from users where user_name = ($1::text) and user_password = ($2::text);";
+  const result = await client.query(query, [username, password]);
+  const userData = result.rows[0];
+  if (userData != undefined) {
+    // console.log(userData);
+    return true;
+  } else {
+    console.log("user does not exist");
+  }
+};
+
 exports.getAllItems = getAllItems;
 exports.addNewItem = addNewItem;
+exports.markDone = markDone;
+exports.markUndo = markUndo;
+exports.deleteItem = deleteItem;
+exports.addNewUser = addNewUser;
+exports.getUser = getUser;
+
+// let d = async (e, b) => {
+//   const s = await getUser(e, b);
+//   console.log(s, 'this is d');
+// };
+
+// d("B", "m");
