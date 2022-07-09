313e8c2b923c86b28a9a5b7a27fcafbbe4763702
beyarDev <beyarDev@gmail.com>
2022-07-09 09:04:08 +0100

    configure cors to accept all requests

--- src/api.js
+++ src/api.js
@@ -2,11 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const data = require("./data");
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
